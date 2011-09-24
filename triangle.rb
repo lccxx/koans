e4b6aceb210822cdd2112d10443ba648b8d8bdf2
liu-chong <liuchong14@gmail.com>
2011-09-25 04:00:10 +0800

    Done test of about_triangle_project_2.rb

--- triangle.rb
+++ triangle.rb
@@ -14,10 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError, 'not triangle' if a <= 0 or b <= 0 or c <= 0
+  raise TriangleError, 'not triangle' if not (a + b > c and (a - b).abs < c )
+
   return :equilateral if a == b and b == c
-
   return :isosceles if [a, b, c].uniq.size == 2
-
   :scalene
 end
 
